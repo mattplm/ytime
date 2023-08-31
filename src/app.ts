3150541fb50525825418cb4de599a7a3f7225ba7
mattplm <hello@mattplm.fr>
2023-08-31 16:44:40 +0200

    Move numberToDDHHMMSS to a utils.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,4 @@
-const numberToDDHHMMSS = (value: number): string => {
-  let hours      = Math.floor(value / 3600),
-      days       = Math.floor(hours / 24),
-      minutes    = Math.floor((value - (hours * 3600)) / 60),
-      seconds    = Math.floor(value - (hours * 3600) - (minutes * 60)),
-      ddhhmmss   = [],
-      daysStr    = String(days),
-      hoursStr   = String(hours),
-      minutesStr = String(minutes),
-      secondsStr = String(seconds)
-
-  if (days) {
-    ddhhmmss.push(daysStr + ':')
-    hours -= days * 24
-    if (hours < 10) { hoursStr = '0' + hours }
-  }
-  if (days || hours) {
-    ddhhmmss.push(hoursStr)
-    if (minutes < 10) {minutesStr = '0' + minutes}
-  }
-  ddhhmmss.push(minutesStr)
-  if (seconds < 10) {secondsStr = '0' + seconds}
-  ddhhmmss.push(secondsStr)
-  return ddhhmmss.join(':')
-}
+import { numberToDDHHMMSS } from './utils'
 
 class YTiExtension {
   public getVideoElement(): HTMLVideoElement | null {
@@ -42,6 +18,7 @@ class YTiExtension {
     }
 
     displayer.textContent = text
+    console.debug(video?.playbackRate)
     if (video?.playbackRate || 1 !== 1) {
       const actualTime = timeWithPlaybackRate(video?.duration || 0, video?.playbackRate || 1)
       displayer.textContent = text + ' (x' + video?.playbackRate + ': ' + numberToDDHHMMSS(actualTime) + ')'
---
+++ src/utils.ts
@@ -0,0 +1,25 @@
+export function numberToDDHHMMSS(value: number): string {
+  let hours      = Math.floor(value / 3600),
+      days       = Math.floor(hours / 24),
+      minutes    = Math.floor((value - (hours * 3600)) / 60),
+      seconds    = Math.floor(value - (hours * 3600) - (minutes * 60)),
+      ddhhmmss   = [],
+      daysStr    = String(days),
+      hoursStr   = String(hours),
+      minutesStr = String(minutes),
+      secondsStr = String(seconds)
+
+  if (days) {
+    ddhhmmss.push(daysStr + ':')
+    hours -= days * 24
+    if (hours < 10) { hoursStr = '0' + hours }
+  }
+  if (days || hours) {
+    ddhhmmss.push(hoursStr)
+    if (minutes < 10) {minutesStr = '0' + minutes}
+  }
+  ddhhmmss.push(minutesStr)
+  if (seconds < 10) {secondsStr = '0' + seconds}
+  ddhhmmss.push(secondsStr)
+  return ddhhmmss.join(':')
+}
